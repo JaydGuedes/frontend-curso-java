9716264daab44436ced041f4e2530e9679a0e68a
Jaydee Guedes <jayd.paiva@gmail.com>
2023-04-17 21:30:59 -0300

    Correção-do-bug

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,21 @@
 $(document).ready(myHome)
+
+/**
+ * URL para obter todos os artigos ordenados pela data:
+ * http://localhost:3000/articles?_sort=date&_order=desc
+ */
+
 /**
  * Função principal da página "home".
  **/
- function myHome() {
-  changeTitle()
+function myHome() {
+    /**
+     * Altera o título da página quando 'home' for acessada.
+     **/
+    changeTitle()
+
+    $.get(app.apiArticlesURL)
+        .done((data) =>{
+            console.log(data)
+        })
 }
--- pages/policies/index.js
+++ pages/policies/index.js
@@ -1,12 +1,9 @@
 $(document).ready(myPolicies)
 
-/**
- * Função principal da página "e404".
- **/
 function myPolicies() {
-    /**
-     * Altera o título da página quando 'e404' for acessada.
-     **/
-    console.log('Oba! policies funciona')
-    changeTitle('Policies')
+    
+    changeTitle('Políticas de Privacidade')
+    
+    console.log('Oba! policies funciona!')
+
 }
